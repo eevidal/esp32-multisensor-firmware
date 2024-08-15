6140c08f6070bfbbb0abbd38e05ed07b0c033508
Erica Vidal <ericavidal@gmail.com>
2024-08-15 13:34:53 -0300

    update i2c routines

--- mcu_interfaces/include/i2c_module.h
+++ mcu_interfaces/include/i2c_module.h
@@ -14,9 +14,10 @@ typedef struct w_i2c_config_t
 } w_i2c_config_t;
 
 typedef void* i2c_bus_t;
+typedef void* i2c_dev_t;
 
 
-/**
+ /**
  * @brief Initializes an I2C bus in master mode.
  *
  * This function configures the specified I2C pins with pull-up resistors and initializes
@@ -26,40 +27,72 @@ typedef void* i2c_bus_t;
  *   - params->sda_num: SDA pin number.
  *   - params->scl_num: SCL pin number.
  *   - params->clk_speed: I2C clock speed in Hz.
- * @return A pointer to the initialized I2C bus, or NULL on failure.
+ * @return A pointer to the initialized I2C bus handle, or NULL on error.
  */
-void * i2c_init(w_i2c_config_t *params);
+i2c_bus_t * i2c_init_bus(w_i2c_config_t *params);
 
-err_t i2c_add_master_device(uint8_t is, uint16_t dev_addr, uint32_t cl_speed, void* bus_handle,void *dev);
 /**
- * @brief Reads multiple bytes from an I2C slave device.
+ * Adds a master device to an I2C bus.
  *
- * Reads the specified number of bytes from the given I2C slave device.
+ * This function adds a master device to the specified I2C bus.
  *
- * @param dev_addr The 7-bit address of the slave device.
- * @param length The number of bytes to read.
- * @param data Pointer to a buffer to store the read data.
- * @param timeout Maximum time in milliseconds to wait for the read operation.
- *
- * @return OK on success, or an error code on failure.
+ * @param is_seven Indicates whether to use a 7-bit or 10-bit device address.
+ * @param dev_addr The device address.
+ * @param cl_speed The clock speed in Hz.
+ * @param bus_handle A pointer to the I2C bus handle.
+ * @param dev A pointer to store the device handle.
+ * @return E_OK on success, error code otherwise.
  */
+err_t i2c_add_master_device(uint8_t is, uint16_t dev_addr, uint32_t cl_speed, void* bus_handle,void *dev);
 
+/**
+ * Reads data from an I2C device register.
+ *
+ * This function reads a specified number of bytes from a register on the given I2C device.
+ *
+ * @param dev_handler A pointer to the device handle.
+ * @param reg_addr The register address to read from.
+ * @param length The number of bytes to read.
+ * @param data A pointer to the buffer where the read data will be stored.
+ * @return E_OK on success, error code otherwise.
+ *
+ * @note The function currently overwrites the input data pointer with the internal buffer address.
+ *       Consider using a separate buffer for storing the read data to avoid unexpected behavior.
+ */
 err_t i2c_read(void * dev_handler, uint8_t reg_addr,  uint8_t length, uint8_t *data);
 
+
 /**
- * @brief Writes multiples bytes from an I2C slave device.
+ * Writes data to an I2C device register.
  *
- * Writes multiples bytes of data from the specified I2C slave device.
+ * This function writes a specified number of bytes to a register on the given I2C device.
  *
- * @param dev_addr The 7-bit address of the slave device.
- * @param data Pointer to a buffer with the data to send on the bus.
- * @param length Size, in bytes, of the write buffer.
- * @param timeout Maximum time in milliseconds to wait for the write operation.
- *
- * @return OK on success, or an error code on failure.
+ * @param dev_handler A pointer to the device handle.
+ * @param reg_addr The register address to write to.
+ * @param data The data to be written.
+ * @param length The number of bytes to write.
+ * @return E_OK on success, error code otherwise.
  */
 err_t i2c_write(void * dev_handler, uint8_t reg_addr, uint8_t *data, uint8_t length);
 
+
+/**
+ * Deletes a master device from an I2C bus.
+ *
+ * This function removes a master device from the specified I2C bus.
+ *
+ * @param dev_handler A pointer to the device handle.
+ * @return E_OK on success, error code otherwise.
+ */
 err_t i2c_del_master_device(void * dev_handler);
+
+/**
+ * Deinitializes an I2C bus. Is not threads safe.
+ *
+ * This function deinitializes the specified I2C bus.
+ *
+ * @param bus_handler A pointer to the bus handle.
+ * @return E_OK on success, error code otherwise.
+ */
 err_t i2c_deinit(void * bus_handler);
 #endif
--- mcu_interfaces/src/i2c_module.c
+++ mcu_interfaces/src/i2c_module.c
@@ -4,77 +4,78 @@
 #include "esp_err.h"
 #include "i2c_module.h"
 #include "error_module.h"
-
+#include <string.h>
 
 #ifndef CONFIG_FREERTOS_HZ
 #define CONFIG_FREERTOS_HZ 100  
 #endif
+
 #define I2C_NUM I2C_NUM_0
 #define NULL_I2C_MEM_ADDR 0xFF   
 #define I2C_ACK_CHECK_EN 0x1   
 
-typedef struct{
 
 
-} i2c_dev_t;
-
-
-void * i2c_init_bus(w_i2c_config_t *params){
+i2c_bus_t * i2c_init_bus(const w_i2c_config_t *params)
+{
     i2c_master_bus_config_t conf ;
     conf.i2c_port = -1; //autoconfig
-    conf.clk_source = I2C_CLK_SRC_DEFAULT,
+    conf.clk_source = I2C_CLK_SRC_DEFAULT;
     conf.sda_io_num = params->sda_num;
     conf.scl_io_num = params->scl_num;
     conf.glitch_ignore_cnt = 7;
     conf.flags.enable_internal_pullup = true;
     i2c_master_bus_handle_t bus_handle;
     ESP_ERROR_CHECK(i2c_new_master_bus(&conf, &bus_handle));
-    return (void *)bus_handle;
-   
-}
+    return (i2c_bus_t *)bus_handle;  
+};
 
-err_t i2c_add_master_device(uint8_t is, uint16_t dev_addr, uint32_t cl_speed, void* bus_handle,void *dev)
+
+err_t i2c_add_master_device(uint8_t is_seven, uint16_t dev_addr, uint32_t cl_speed, i2c_bus_t* bus_handle, i2c_dev_t *dev)
 {
     i2c_device_config_t cfg;
-    cfg.dev_addr_length = (is!=0) ? I2C_ADDR_BIT_LEN_7 : I2C_ADDR_BIT_LEN_10;
+    cfg.dev_addr_length = (is_seven!=0) ? I2C_ADDR_BIT_LEN_7 : I2C_ADDR_BIT_LEN_10;
     cfg.device_address = dev_addr;
     cfg.scl_speed_hz = cl_speed;
     i2c_master_dev_handle_t dev_handle;
     i2c_master_bus_handle_t * bus = (i2c_master_bus_handle_t *) bus_handle;
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus, &cfg, &dev_handle));
-    void* dev = &dev_handle;
+    i2c_dev_t* dev = &dev_handle;
     return E_OK;
 };
 
 
-err_t i2c_write(void * dev_handler, uint8_t reg_addr, uint8_t *data, uint8_t length){
+err_t i2c_write(void * dev_handler, uint8_t reg_addr, const uint8_t *data, uint8_t length)
+{
     uint8_t reg_and_data[length + 1];
     reg_and_data[0] = reg_addr;
     memcpy(&(reg_and_data[1]), data, length);
     i2c_master_dev_handle_t dev = *(i2c_master_dev_handle_t *)dev_handler;
     ESP_ERROR_CHECK(i2c_master_transmit(dev, reg_and_data, length + 1,-1));
     return E_OK;
-}
+};
 
 err_t i2c_read(void * dev_handler, uint8_t reg_addr,  uint8_t length, uint8_t *data)
 {
     uint8_t reg_and_data[length + 1];
     reg_and_data[0] = reg_addr;
-    *data = &reg_and_data; 
+    data = &reg_and_data; 
     i2c_master_dev_handle_t dev = *(i2c_master_dev_handle_t *)dev_handler;
     ESP_ERROR_CHECK(i2c_master_receive(dev, data, length+1, -1));
     return E_OK;
     
-}
+};
 
-err_t i2c_del_master_device(void * dev_handler){
+err_t i2c_del_master_device(void * dev_handler)
+{
     i2c_master_dev_handle_t dev = *(i2c_master_dev_handle_t *)dev_handler;
     ESP_ERROR_CHECK(i2c_master_bus_rm_device(dev));
     return E_OK;
-}
+};
 
-err_t i2c_deinit(void * bus_handler){
+err_t i2c_deinit(void * bus_handler)
+{
     i2c_master_bus_handle_t * bus = (i2c_master_bus_handle_t *) bus_handler;
     ESP_ERROR_CHECK(i2c_del_master_bus(bus));
     return E_OK;
-}
+};
