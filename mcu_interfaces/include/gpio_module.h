6ec13ddb2ce8e15d5ac83f2fcb2999bf1e5c769d
Erica Vidal <ericavidal@gmail.com>
2024-08-28 18:00:25 -0300

    doc: complete gpio comments

--- mcu_interfaces/include/gpio_module.h
+++ mcu_interfaces/include/gpio_module.h
@@ -9,9 +9,51 @@ typedef enum {
     GPIO_OUTPUT = 2
 } direction_t;
 
+/**
+ * Initializes a GPIO pin.
+ *
+ * This function configures a GPIO pin for input or output, enabling or disabling
+ * pull-up and pull-down resistors.
+ *
+ * @param pin The GPIO pin number.
+ * @param mode The GPIO mode:
+ *   - GPIO_INPUT: Input mode.
+ *   - GPIO_OUTPUT: Output mode.
+ * @return E_OK on success.
+ */
 err_t gpio_init(uint8_t gpio_pin, direction_t mode);
+
+/**
+ * Sets the direction of a GPIO pin.
+ *
+ * This function configures a GPIO pin for input or output.
+ *
+ * @param pin The GPIO pin number.
+ * @param mode The GPIO mode:
+ *   - GPIO_INPUT: Input mode.
+ *   - GPIO_OUTPUT: Output mode.
+ * @return E_OK on success.
+ */
 err_t gpio_set(uint8_t gpio_pin, direction_t mode);
+
+/**
+ * Sets a GPIO pin to HIGH.
+ *
+ * This function sets the specified GPIO pin to a high level.
+ *
+ * @param gpio_pin The GPIO pin number.
+ * @return E_OK on success.
+ */
 err_t gpio_send(uint8_t gpio_pin);
+
+/**
+ * Sets a GPIO pin to LOW.
+ *
+ * This function sets the specified GPIO pin to a low level.
+ *
+ * @param gpio_pin The GPIO pin number.
+ * @return E_OK on success.
+ */
 err_t gpio_stop(uint8_t gpio_pin);
 int gpio_read(uint8_t gpio_pin);
 
--- mcu_interfaces/src/gpio_module.c
+++ mcu_interfaces/src/gpio_module.c
@@ -10,41 +10,36 @@
 #define LOW 0
 #define HIGH 1
 
+err_t gpio_init(uint8_t pin, direction_t mode)
+{
+    gpio_config_t gpio_conf;
+    gpio_conf.pin_bit_mask = (1ULL << pin); /*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
+    gpio_conf.mode = (gpio_mode_t)mode;
+    gpio_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_config(&gpio_conf));
+    return E_OK;
+};
 
-    err_t gpio_init(uint8_t pin, direction_t mode)
-    {
-        gpio_config_t gpio_conf;
-        gpio_conf.pin_bit_mask = (1ULL << pin); /*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
-        gpio_conf.mode = (gpio_mode_t)mode;
-        gpio_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-        gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-        ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_config(&gpio_conf));
-        return E_OK;
-    };
+err_t gpio_set(uint8_t pin, direction_t mode)
+{
+    ESP_ERROR_CHECK(gpio_set_direction((gpio_num_t)pin, (gpio_mode_t)mode));
+    return E_OK;
+}
 
-    err_t gpio_set(uint8_t pin, direction_t mode){
-        ESP_ERROR_CHECK(gpio_set_direction((gpio_num_t)pin, (gpio_mode_t)mode));
-        return E_OK;
-    }
+err_t gpio_send(uint8_t gpio_pin)
+{
+    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)gpio_pin, (uint32_t)HIGH));
+    return E_OK;
+};
 
-    err_t gpio_send(uint8_t gpio_pin)
-    {   
- 
+err_t gpio_stop(uint8_t gpio_pin)
+{
+    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)gpio_pin, (uint32_t)LOW));
+    return E_OK;
+};
 
-        ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)gpio_pin, (uint32_t)HIGH));
-
-
-        return E_OK;
-    };
-
-    err_t gpio_stop(uint8_t gpio_pin)
-    {
-        ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)gpio_pin, (uint32_t)LOW));
-        return E_OK;
-    };
-
-    int gpio_read(uint8_t gpio_pin)
-    {
-           return gpio_get_level((gpio_num_t)gpio_pin);
-    };
-
+int gpio_read(uint8_t gpio_pin)
+{
+    return gpio_get_level((gpio_num_t)gpio_pin);
+};
