558b4b2697fdbab1b225a03d7508d60ccda7556b
Erica Vidal <ericavidal@gmail.com>
2024-08-31 18:21:56 -0300

    doc: add Doxyfile

--- mcu_interfaces/include/mutex_module.h
+++ mcu_interfaces/include/mutex_module.h
@@ -4,8 +4,33 @@
 
 typedef void* mutex_t;
 
+/**
+ * Initializes a mutex.
+ *
+ * This function creates a binary semaphore and returns a pointer to it, which can be used as a mutex.
+ * If memory allocation fails, the function returns NULL.
+ *
+ * @return A pointer to the newly created mutex, or NULL if memory allocation fails.
+ */
 mutex_t mutex_init(void);
+
+/**
+ * Acquires a mutex lock.
+ *
+ * This function blocks the calling thread until the mutex is available. 
+ * Once acquired, the mutex is locked, preventing other threads from acquiring it.
+ *
+ * @param mutex A pointer to the mutex to be locked.
+ */
 void mutex_lock(mutex_t *mu);
+
+/**
+ * Releases a mutex lock.
+ *
+ * This function releases the mutex lock, allowing other threads to acquire it.
+ *
+ * @param mutex A pointer to the mutex to be unlocked.
+ */
 void mutex_unlock(mutex_t *mu);
 
 #endif
--- mcu_interfaces/include/time_module.h
+++ mcu_interfaces/include/time_module.h
@@ -5,14 +5,26 @@
 
 
 /**
- * @brief 
- * 
- * @param time 
+ * Delays execution for a specified number of microseconds.
+ *
+ * @param time The delay duration in microseconds.
  */
 void delay_us(int time);
 
+/**
+ * Gets the current time in microseconds.
+ * 
+ * @return The current time in microseconds.
+ */
 uint64_t  now(void);
 
+/**
+ * Calculates the elapsed time in microseconds since a given start time.
+ * This function subtracts the start time from the current time to determine the elapsed time in microseconds.
+ *
+ * @param start_time The start time in microseconds.
+ * @return The elapsed time in microseconds.
+ */
 uint64_t elapsed_time(uint64_t time);
 
 #endif
